42cf41172db20fc53a43240860d990b39596fd5e
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-06-17 20:09:29 -0400

    fetch(plugin) delete files

--- IncreaseSpeedPlugin/src/main/java/org/plugins/minevidencia/Exeception/InvalidCommandExeception.java
+++
@@ -1,7 +0,0 @@
-package org.plugins.minevidencia.Exeception;
-
-public class InvalidCommandExeception extends RuntimeException {
-  public InvalidCommandExeception(String message) {
-    super("Invalid user command: " + message);
-  }
-}
